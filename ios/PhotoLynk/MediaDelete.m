98a71291c6f1d255013df0ac79edf816362012c2
StealthLynk.io <support@stealthlynk.io>
2026-03-03 10:43:35 +0200

    Remove iOS folder from public repo - Android-only app for hackathon

--- ios/PhotoLynk/MediaDelete.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(MediaDelete, NSObject)
-
-RCT_EXTERN_METHOD(deleteAssets:(NSArray *)assetIds
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-
-@end
